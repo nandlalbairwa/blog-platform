bf2861540bba0e916918634d942209b781748b65
Nandu <nandlalbairwa18@gmail.com>
2026-03-01 14:30:23 +0530

    Added login js and updated index

---
+++ frontend/login.js
@@ -0,0 +1,8 @@
+document.getElementById("loginBtn").addEventListener("click", async () => {
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  console.log("Clicked"); // test ke liye
+
+});
